100faf87a1fdb8475e04a675bad3ce83b3efdadb
Niklas Schmidtmer <ns@ad2games.com>
2014-06-13 16:33:37 +0200

    Keep the non-admin API versions when regenerating the file

--- script/update_versions.rb
+++ script/update_versions.rb
@@ -17,7 +17,7 @@ request = Net::HTTP::Get.new(uri.request_uri)
 response = http.request(request)
 
 html = Nokogiri::HTML.parse(response.body)
-versions = { admin: {} }
+versions = {}
 
 html.css('div.article-body table').each do |table|
   section_head = table.previous_element
@@ -26,14 +26,16 @@ html.css('div.article-body table').each do |table|
     section_head = section_head.previous_element
   end
   section = section_head.text[/\w+/].downcase.to_sym
-  versions[:admin][section] = {}
+  versions[section] = {}
   table.css('tr')[1..-1].each do |row|
     version, method = row.css('td').map(&:text)
     method = method[/\w+/]
     next if method.blank?
     method = method.underscore.to_sym
-    versions[:admin][section][method] = version.to_i
+    versions[section][method] = version.to_i
   end
 end
 
-File.open('api_versions.yml', 'w') { |f| f << YAML.dump(versions) }
+yaml = YAML.load_file('api_versions.yml')
+yaml[:admin] = versions
+File.open('api_versions.yml', 'w') { |f| f << YAML.dump(yaml) }
