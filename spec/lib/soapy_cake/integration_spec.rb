dba6a2603e7630a40465e4f07fc31fb59f1f7b7d
Niko Dziemba <niko@dziemba.com>
2015-02-12 13:17:35 +0100

    Add more integration tests

--- spec/lib/soapy_cake/integration_spec.rb
+++ spec/lib/soapy_cake/integration_spec.rb
@@ -15,6 +15,24 @@ RSpec.describe 'Integration test' do
       # floats
       minimum_payment_threshold: '0.0000',
     )
+
+    # arrays
+    expect(result.first[:contacts][:contact_info].map { |contact| contact[:contact_id]}).to \
+      eq([8819, 8820])
+  end
+
+  it 'returns a clicks report with a defined time range', :vcr do
+    result = SoapyCake::Admin.new.clicks(
+      start_date: Date.new(2014, 6, 30),
+      end_date: Date.new(2014, 7, 1),
+      row_limit: 1
+    )
+
+    expect(result.count).to eq(1)
+    expect(result.first).to include(
+      click_id: 1275452,
+      visitor_id: 1208222
+    )
   end
 
   it 'raises if there is an error', :vcr do
