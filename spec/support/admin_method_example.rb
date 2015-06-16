3759f2984bd165a738ef89d949decabb98cd57e5
Niko Dziemba <niko@dziemba.com>
2015-06-16 14:29:01 +0200

    Do not accept wildcard parametes for GET methods

--- spec/support/admin_method_example.rb
+++ spec/support/admin_method_example.rb
@@ -2,9 +2,9 @@ RSpec.shared_examples_for 'a cake admin method' do
   it 'runs the request' do
     request = double('request')
     expect(SoapyCake::Request).to receive(:new)
-      .with(:admin, service, cake_method, cake_opts).and_return(request)
+      .with(:admin, service, cake_method, cake_opts || {}).and_return(request)
     expect(subject).to receive(:run).with(request)
 
-    subject.public_send(method, cake_opts)
+    subject.public_send(method, *[cake_opts].compact)
   end
 end
