1534acb27deaf93c8557d5d2e850fe55eed4b962
Guilherme Goettems Schneider <guigs81@gmail.com>
2017-07-24 16:45:14 -0300

    Upgrade MassConversionInsert API to version 2

--- spec/integration/soapy_cake/admin_track_spec.rb
+++ spec/integration/soapy_cake/admin_track_spec.rb
@@ -8,12 +8,16 @@ RSpec.describe SoapyCake::AdminTrack do
   describe '#mass_conversion_insert', :vcr do
     it 'inserts conversions' do
       result = admin_track.mass_conversion_insert(
-        conversion_date: Date.new(2015, 5, 6),
+        conversion_date: Date.new(2015, 5, 7),
         affiliate_id: 16059,
         campaign_id: 13268,
         sub_affiliate: '',
         creative_id: 5521,
-        total_to_insert: 12
+        total_to_insert: 3,
+        note: "Test created on #{Date.current}",
+        payout: 0.1,
+        received: 0.2,
+        transaction_ids: (1..3).map { |i| "test-transaction-id-#{i}" }.join(',')
       )
 
       expect(result).to eq(success: true, message: 'Conversions Inserted')
