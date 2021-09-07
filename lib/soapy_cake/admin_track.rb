8509ee783f8051c980189a3c1ca68a4e796862be
modullar <mouazq@gmail.com>
2021-09-07 17:08:06 +0200

    Update to rails 6.1.4 (#77)
    * update ruby and rails version
    
    * update ruby version of circleci
    
    * fix time offset
    
    * update actoinpack to 6.1.4
    
    * fix errors
    
    * upgrade to ruby 2.6.1
    
    * Revert "upgrade to ruby 2.6.1"
    
    This reverts commit 12efe961302fa805a1c3c8b96a93fe1dba9e13e8.
    
    * unristrict rails version

--- lib/soapy_cake/admin_track.rb
+++ lib/soapy_cake/admin_track.rb
@@ -20,7 +20,6 @@ module SoapyCake
                        conversion_date affiliate_id sub_affiliate
                        campaign_id creative_id total_to_insert
                      ])
-
       run Request.new(:admin, :track, :mass_conversion_insert, opts)
     end
 
